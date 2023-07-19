57a4c6b1a892c9e66dc356a5297addfd88d94507
cheezeman <pksam5046@naver.com>
2023-07-19 11:34:59 +0900

    edit js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,14 @@
 document.addEventListener("keydown", function (e) {
-  const heroLeft = getComputedStyle(heroElement).left;
-  console.log("용사의 왼쪽 값 =>", heroLeft);
+  const heroLeft = heroElement.style.left;
+  // console.log("용사의 왼쪽 값 =>", heroLeft);
+
+  const heroLeftWithoutPx = parseInt(heroLeft.split("px")[0]);
 
   if (e.keyCode === 37) {
-    heroElement.style.left = "0px";
+    heroElement.style.left = heroLeftWithoutPx - 10 + "px";
+    // console.log(heroLeftWithoutPx - 1);
   } else if (e.keyCode === 39) {
-    heroElement.style.left = "765px";
+    heroElement.style.left = heroLeftWithoutPx + 10 + "px";
+    // console.log(heroLeftWithoutPx + 1);
   }
 });
